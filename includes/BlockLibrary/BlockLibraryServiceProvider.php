2832ed11eeaa9b77109e45611bd0c31b0685c5c1
JR Tashjian <hello@jrtashjian.com>
2025-11-26 19:06:43 -0500

    Add list and grid block patterns

--- includes/BlockLibrary/BlockLibraryServiceProvider.php
+++ includes/BlockLibrary/BlockLibraryServiceProvider.php
@@ -48,6 +48,14 @@ class BlockLibraryServiceProvider extends AbstractServiceProvider implements Boo
 	 */
 	public function boot(): void {
 		add_action( 'init', array( $this, 'register_blocks' ) );
+
+		// Only register block patterns if the api_key and user settings are set.
+		if (
+			get_option( 'profile_blocks_lastfm_api_key' ) ||
+			get_option( 'profile_blocks_lastfm_profile' )
+		) {
+			add_action( 'init', array( $this, 'register_block_patterns' ) );
+		}
 	}
 
 	/**
@@ -72,5 +80,122 @@ class BlockLibraryServiceProvider extends AbstractServiceProvider implements Boo
 				)
 			);
 		}
+	}
+
+	public function register_block_patterns() {
+		register_block_pattern_category(
+			'profile-blocks',
+			array( 'label' => __( 'Profile Blocks', 'profile-blocks-lastfm' ) )
+		);
+
+		$grid_template = function ( $type, $label ) {
+			$prop_key = rtrim( $type, 's' );
+			return '
+                <!-- wp:profile-blocks-lastfm/top-charts {"collection":"' . esc_attr( $type ) . '","period":"1month","itemsToShow":8} -->
+                <!-- wp:heading {"level":3} -->
+                <h3 class="wp-block-heading">' . esc_html( $label ) . '</h3>
+                <!-- /wp:heading -->
+
+                <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"left","orientation":"horizontal"}} -->
+                <div class="wp-block-group"><!-- wp:profile-blocks-lastfm/dynamic-template {"style":{"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"flex","justifyContent":"left"}} -->
+                <!-- wp:profile-blocks-lastfm/item-image {"itemImageProp":"' . esc_attr( $prop_key ) . '.images","itemLinkProp":"' . esc_attr( $prop_key ) . '.url","itemImageSize":"large","width":174,"isLink":true,"style":{"layout":{"selfStretch":"fill","flexSize":null},"color":[]}} /-->
+                <!-- /wp:profile-blocks-lastfm/dynamic-template --></div>
+                <!-- /wp:group -->
+                <!-- /wp:profile-blocks-lastfm/top-charts -->
+            ';
+		};
+
+		register_block_pattern(
+			'profile-blocks-lastfm/top-artists-grid',
+			array(
+				'title'         => __( 'Top Artists Grid', 'profile-blocks-lastfm' ),
+				'blockTypes'    => array( 'profile-blocks-lastfm/top-charts' ),
+				'categories'    => array( 'profile-blocks' ),
+				'viewportWidth' => 696,
+				'content'       => $grid_template( 'artists', __( 'Top Artists', 'profile-blocks-lastfm' ) ),
+			)
+		);
+
+		register_block_pattern(
+			'profile-blocks-lastfm/top-albums-grid',
+			array(
+				'title'         => __( 'Top Albums Grid', 'profile-blocks-lastfm' ),
+				'blockTypes'    => array( 'profile-blocks-lastfm/top-charts' ),
+				'categories'    => array( 'profile-blocks' ),
+				'viewportWidth' => 696,
+				'content'       => $grid_template( 'albums', __( 'Top Albums', 'profile-blocks-lastfm' ) ),
+			)
+		);
+
+		register_block_pattern(
+			'profile-blocks-lastfm/top-tracks-grid',
+			array(
+				'title'         => __( 'Top Tracks Grid', 'profile-blocks-lastfm' ),
+				'blockTypes'    => array( 'profile-blocks-lastfm/top-charts' ),
+				'categories'    => array( 'profile-blocks' ),
+				'viewportWidth' => 696,
+				'content'       => $grid_template( 'tracks', __( 'Top Tracks', 'profile-blocks-lastfm' ) ),
+			)
+		);
+
+		$list_template = function ( $type, $label ) {
+			$prop_key = rtrim( $type, 's' );
+			return '
+                <!-- wp:profile-blocks-lastfm/top-charts {"collection":"' . esc_attr( $type ) . '","period":"1month"} -->
+                <!-- wp:heading {"level":3} -->
+                <h3 class="wp-block-heading">' . esc_html( $label ) . '</h3>
+                <!-- /wp:heading -->
+
+                <!-- wp:profile-blocks-lastfm/dynamic-template {"layout":{"type":"flex"}} -->
+                <!-- wp:profile-blocks-lastfm/item-image {"itemImageProp":"' . esc_attr( $prop_key ) . '.images","itemLinkProp":"' . esc_attr( $prop_key ) . '.url"} /-->
+
+                <!-- wp:group {"style":{"layout":{"selfStretch":"fill"},"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap","orientation":"vertical"}} -->
+                <div class="wp-block-group"><!-- wp:profile-blocks-lastfm/item-name {"itemTextProp":"' . esc_attr( $prop_key ) . '.name","itemLinkProp":"' . esc_attr( $prop_key ) . '.url","isLink":true,"style":{"typography":{"fontStyle":"normal","fontWeight":700}}} /--></div>
+                <!-- /wp:group -->
+
+                <!-- wp:group {"style":{"spacing":{"blockGap":"0.25em"},"typography":{"fontSize":"0.8em"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+                <div class="wp-block-group" style="font-size:0.8em"><!-- wp:profile-blocks-lastfm/item-name {"itemTextProp":"' . esc_attr( $prop_key ) . '.playcount"} /-->
+
+                <!-- wp:paragraph -->
+                <p>plays</p>
+                <!-- /wp:paragraph --></div>
+                <!-- /wp:group -->
+                <!-- /wp:profile-blocks-lastfm/dynamic-template -->
+                <!-- /wp:profile-blocks-lastfm/top-charts -->
+            ';
+		};
+
+		register_block_pattern(
+			'profile-blocks-lastfm/top-artists-list',
+			array(
+				'title'         => __( 'Top Artists List', 'profile-blocks-lastfm' ),
+				'blockTypes'    => array( 'profile-blocks-lastfm/top-charts' ),
+				'categories'    => array( 'profile-blocks' ),
+				'viewportWidth' => 696,
+				'content'       => $list_template( 'artists', __( 'Top Artists', 'profile-blocks-lastfm' ) ),
+			)
+		);
+
+		register_block_pattern(
+			'profile-blocks-lastfm/top-albums-list',
+			array(
+				'title'         => __( 'Top Albums List', 'profile-blocks-lastfm' ),
+				'blockTypes'    => array( 'profile-blocks-lastfm/top-charts' ),
+				'categories'    => array( 'profile-blocks' ),
+				'viewportWidth' => 696,
+				'content'       => $list_template( 'albums', __( 'Top Albums', 'profile-blocks-lastfm' ) ),
+			)
+		);
+
+		register_block_pattern(
+			'profile-blocks-lastfm/top-tracks-list',
+			array(
+				'title'         => __( 'Top Tracks List', 'profile-blocks-lastfm' ),
+				'blockTypes'    => array( 'profile-blocks-lastfm/top-charts' ),
+				'categories'    => array( 'profile-blocks' ),
+				'viewportWidth' => 696,
+				'content'       => $list_template( 'tracks', __( 'Top Tracks', 'profile-blocks-lastfm' ) ),
+			)
+		);
 	}
 }
